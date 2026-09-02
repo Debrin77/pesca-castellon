3b8179dc5f5414f116964fd6c0c5c06a5d8f7a5b
Debrin77 <priocas@protonmail.com>
2026-09-02 09:27:20 +0200

    Fix tabBarIcon syntax in App.tsx

--- App.tsx
+++ App.tsx
@@ -97,7 +97,7 @@ export default function App() {
             elevation: 10,
           },
           tabBarLabelStyle: { fontSize: 10.5, fontWeight: "700" as const },
-           tabBarIcon: ({ color, focused, size }) => ( <TabIcon nombre={ICONOS[route.name] ?? "home"} size={focused ? size + 2 : size} color={color} /> )
+           tabBarIcon: ({ color, focused, size }) => ( <TabIcon nombre={ICONOS[route.name] ?? "home"} size={focused ? size + 2 : size} color={color} /> ),
           ),
         })}
       >
